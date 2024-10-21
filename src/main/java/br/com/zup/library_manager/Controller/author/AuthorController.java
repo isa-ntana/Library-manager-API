732a939e3226e900e18f0490fdb698ac339915c7
isabela santana <isabela.santana.prof@gmail.com>
2024-10-21 14:51:25 -0300

    fix details of controllers

--- src/main/java/br/com/zup/library_manager/Controller/author/AuthorController.java
+++ src/main/java/br/com/zup/library_manager/Controller/author/AuthorController.java
@@ -1,4 +1,32 @@
 package br.com.zup.library_manager.Controller.author;
 
+import br.com.zup.library_manager.Controller.author.dtos.AuthorDTO;
+import br.com.zup.library_manager.Models.Author;
+import br.com.zup.library_manager.Service.AuthorService;
+import br.com.zup.library_manager.Service.Mappers.AuthorMapper;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/author")
 public class AuthorController {
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping
+    public List<Author> getAllAuthors() { return authorService.showAuthors(); }
+
+    @GetMapping("/{authorId}")
+    public Author findAuthorById(@PathVariable Long authorId) { return authorService.findAuthorById(authorId); }
+
+    @PostMapping
+    public Author addAuthor(@RequestBody @Valid AuthorDTO authorDTO) {
+        return authorService.saveAuthor(AuthorMapper.fromAuthorDTO(authorDTO));
+    }
+
+    @DeleteMapping("/{authorId}")
+    public void deleteAuthorById(@PathVariable Long authorId) { authorService.deleteAuthor(authorId); }
 }
--- src/main/java/br/com/zup/library_manager/Controller/book/BookController.java
+++ src/main/java/br/com/zup/library_manager/Controller/book/BookController.java
@@ -1,4 +1,8 @@
 package br.com.zup.library_manager.Controller.book;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
 }
